e5fd490dfe1941a4b49651342c33f8f341ccf62c
28sungmin <gooddays4001@naver.com>
2024-03-28 21:42:13 +0900

    finish main

--- js/script.js
+++ js/script.js
@@ -24,3 +24,13 @@ function moveLeft() {
   var nowImg = bgImg[index];
   $year23.style.backgroundImage = `url(${nowImg})`;
 }
+
+// 햄버거 이모티콘 누르기
+var $menu = document.querySelector(".menu");
+
+function openMenu() {
+  $menu.classList.add("active");
+}
+function closeMenu() {
+  $menu.classList.remove("active");
+}
---
+++ js/script_intro.js
@@ -0,0 +1,9 @@
+// 햄버거 이모티콘 누르기
+var $menu = document.querySelector(".menu");
+
+function openMenu() {
+  $menu.classList.add("active");
+}
+function closeMenu() {
+  $menu.classList.remove("active");
+}
